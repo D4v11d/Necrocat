dd02e4704c97e5e1097bd09eafeb994d40b12c9b
D4v11d <dajj.3009@gmail.com>
2025-05-11 13:53:29 -0600

    Initial commit
    - Implement Player movement
    - Enemy movement
    - Player attack vertical hitbox
    - Enemy knockback

---
+++ scripts/player_movement.gd
@@ -0,0 +1,99 @@
+class_name Player extends CharacterBody2D
+
+@onready var animated_sprite_2d: AnimatedSprite2D = $AnimatedSprite2D
+@onready var collision: CollisionShape2D = $CollisionShape2D
+@onready var vertical_hitbox: Area2D = $VerticalAttackHitbox
+@onready var horizontal_hitbox: Area2D = $HorizontalAttackHitbox
+@onready var attack_timer: Timer = $AttackTimer
+
+const SPEED = 100.0
+
+var last_direction := Vector2.DOWN
+var is_moving := false
+var is_attacking := false
+
+func _ready() -> void:
+	animated_sprite_2d.play("idle_front")
+	vertical_hitbox.set_deferred("monitoring", false)
+	horizontal_hitbox.set_deferred("monitoring", false)
+
+func _physics_process(delta: float) -> void:
+	if is_attacking:
+		return
+
+	handle_attack()
+	handle_move(delta)
+
+func handle_move(delta: float) -> void:
+	var direction := Vector2(
+		Input.get_axis("move_left", "move_right"),
+		Input.get_axis("move_up", "move_down")
+	).normalized()
+
+	if direction != Vector2.ZERO:
+		velocity = direction * SPEED * delta
+		update_move_animation(direction)
+		is_moving = true
+	else:
+		velocity = velocity.move_toward(Vector2.ZERO, SPEED)
+		if is_moving and velocity == Vector2.ZERO:
+			play_idle_animation()
+			is_moving = false
+
+	move_and_collide(velocity)
+
+func handle_attack() -> void:
+	if Input.is_action_just_pressed("attack"):
+		is_attacking = true
+
+		if last_direction == Vector2.UP:
+			animated_sprite_2d.play("attack_back")
+			activate_hitbox(vertical_hitbox, Vector2(0, -16))
+		elif last_direction == Vector2.DOWN:
+			animated_sprite_2d.play("attack_front")
+			activate_hitbox(vertical_hitbox, Vector2(0, 16))
+		else:
+			animated_sprite_2d.play("attack_side")
+			animated_sprite_2d.flip_h = last_direction == Vector2.LEFT
+			activate_hitbox(horizontal_hitbox, Vector2(-16 if last_direction == Vector2.LEFT else 16, 0))
+
+		attack_timer.start()
+
+func activate_hitbox(hitbox: Area2D, offset: Vector2) -> void:
+	hitbox.global_position = global_position + offset
+	hitbox.set_deferred("monitoring", true)
+
+func update_move_animation(direction: Vector2) -> void:
+	if not is_attacking:
+		if abs(direction.y) > abs(direction.x):
+			if direction.y < 0:
+				animated_sprite_2d.play("walk_back")
+				last_direction = Vector2.UP
+			else:
+				animated_sprite_2d.play("walk_front")
+				last_direction = Vector2.DOWN
+		else:
+			if direction.x != 0:
+				animated_sprite_2d.play("walk_side")
+				animated_sprite_2d.flip_h = direction.x < 0
+				last_direction = Vector2.RIGHT if direction.x > 0 else Vector2.LEFT
+
+func play_idle_animation() -> void:
+	if last_direction == Vector2.UP:
+		animated_sprite_2d.play("idle_back")
+	elif last_direction == Vector2.DOWN:
+		animated_sprite_2d.play("idle_front")
+	else:
+		animated_sprite_2d.play("idle_side")
+		animated_sprite_2d.flip_h = last_direction == Vector2.LEFT
+
+func _on_attack_hitbox_body_entered(body: Node2D) -> void:
+	if body is Enemy and vertical_hitbox.visible:
+		body.attack_received(position)
+
+
+func _on_attack_timer_timeout() -> void:
+	vertical_hitbox.set_deferred("monitoring", false)
+	horizontal_hitbox.set_deferred("monitoring", false)
+	is_attacking = false
+	play_idle_animation()
---
+++ scripts/slime_enemy.gd
@@ -0,0 +1,56 @@
+class_name Enemy extends CharacterBody2D
+
+var speed := 40.0
+var entity_type := "enemy"
+var attack_damage := 20
+var player_inside_chase_area := false
+var is_knocked_back := false
+
+@onready var animated_sprite: AnimatedSprite2D = $AnimatedSprite2D
+@onready var chase_player_area: Area2D = $ChaseArea
+@onready var attack_area: Area2D = $AttackArea
+@onready var knockback_timer: Timer = $KnockbackTimer
+@onready var player: Player = $"../Player"
+
+func _ready() -> void:
+	pass
+
+func _physics_process(delta: float) -> void:
+	if not is_knocked_back and player_inside_chase_area:
+		move_towards_player(delta)
+	elif not is_knocked_back:
+		velocity = Vector2.ZERO
+
+	move_and_slide()
+
+func move_towards_player(delta: float) -> void:
+	if is_knocked_back:
+		return
+
+	var direction = (player.position - position).normalized()
+	velocity = speed * direction
+
+	if direction.x > 0:
+		animated_sprite.flip_h = false
+	elif direction.x < 0:
+		animated_sprite.flip_h = true
+
+func attack_received(from_position: Vector2) -> void:
+	print("attack_received")
+	var knockback_direction = (position - from_position).normalized()
+	velocity = knockback_direction * 200.0
+	is_knocked_back = true
+	knockback_timer.start()
+
+func _on_knockback_timer_timeout() -> void:
+	is_knocked_back = false
+	velocity = Vector2.ZERO
+
+func _on_chase_area_body_entered(body: Node2D) -> void:
+	if body is Player:
+		player_inside_chase_area = true
+
+
+func _on_chase_area_body_exited(body: Node2D) -> void:
+	if body is Player:
+		player_inside_chase_area = false
